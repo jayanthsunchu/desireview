b388c25780493e38d7ff5b43a0d245ddc8256410
jsunchu <jayanth.sunchu@gmail.com>
2015-10-08 20:06:37 -0700

    creating and working with angular services

--- desireview/AngularJS/home-index.js
+++ desireview/AngularJS/home-index.js
@@ -1,43 +1,38 @@
 ﻿var module = angular.module('desireview', ['ngCookies', 'ngRoute']);
-module.controller('homeIndexController', ['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location, dataService) {
+module.controller('homeIndexController', ['$scope', '$http', '$cookies', '$location', 'dataService', function($scope, $http, $cookies, $location, dataService) {
     $scope.isBusy = true;
-    $scope.data = [];
-    var defaultOption = $cookies.get("defaultlanguage");
-    if (defaultOption != null) {
-        $scope.selectedItem = defaultOption;
-        $http.get("/api/movies/getbylanguage/" + $scope.selectedItem).then(function (result) {
-            angular.copy(result.data, $scope.data);
-        }, function () {
-            alert("Error");
-        }).then(function () {
-            $scope.isBusy = false;
-        });
-    }
-    else {
+    $scope.data = dataService;
+    
+    
+    dataService.getMovies().then(function () {
+        var defaultOption = $cookies.get("defaultlanguage");
+        if (defaultOption != null) {
+            $scope.selectedItem = defaultOption;
+            dataService.selectedmovies = dataService.movies.filter(function (movie) {
+                if (defaultOption === "All")
+                    return true;
+                else
+                    return movie.MovieLanguage === defaultOption;
+            });
+        }
+    }).then(function () {
 
-        $http.get("/api/movies/get").then(function (result) {
-            angular.copy(result.data, $scope.data);
-        }, function () {
-            alert("Error");
-        }).then(function () {
-            $scope.isBusy = false;
-        });
-    }
-
+    }).then(function () {
+        $scope.isBusy = false;
+    })
+    
     $scope.showReviewPage = function (data) {
-
         //alert(data);
     };
 
     $scope.listOfOptions = ['All', 'Telugu', 'Hindi', 'Tamil'];
 
     $scope.selectedItemChanged = function () {
-        $http.get("/api/movies/getbylanguage/" + $scope.selectedItem).then(function (result) {
-            angular.copy(result.data, $scope.data);
-        }, function () {
-            alert("Error");
-        }).then(function () {
-            $scope.isBusy = false;
+        dataService.selectedmovies = dataService.movies.filter(function (movie) {
+            if ($scope.selectedItem === "All")
+                return true;
+            else
+                return movie.MovieLanguage === $scope.selectedItem;
         });
     }
 
@@ -45,29 +40,81 @@ module.controller('homeIndexController', ['$scope', '$http', '$cookies', '$locat
         if ($scope.selectedItem != null) {
             $cookies.put("defaultlanguage", $scope.selectedItem);
         }
-
     };
-    $scope.go = function (path) {
+
+    $scope.go = function (path, i) {
+        dataService.selectedmovie = dataService.selectedmovies.filter(function (movie) {
+            return movie.Title === i;
+        });
         $location.path(path);
     };
 }]);
 
+if (!Array.prototype.filter) {
+    Array.prototype.filter = function (fun) {
+        var len = this.length >>> 0;
+        if (typeof fun != "function")
+            throw new TypeError();
+
+        var res = [];
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++) {
+            if (i in this) {
+                var val = this[i]; 
+                if (fun.call(thisp, val, i, this))
+                    res.push(val);
+            }
+        }
+        return res;
+    };
+}
+
+module.controller('reviewIndexController', ['$scope', 'dataService', '$routeParams', function ($scope, dataService, $routeParams) {
+    alert($routeParams.movieTitle);
+    $scope.name = "Review Page";
+    if (dataService == null) {
+        
+    }
+    $scope.data = dataService;
+
+    $scope.reviewdata = [
+        {
+            title: "I call it a great Ripoff! Most of us would call it a Blockbuster!",
+            introduction: "Introduction content. Lorem epsum. Chedsfw sdifjsifs dsfisjdf. Lorem epsuem.. This Review has also ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif. dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf. ksjdi dsfjiwsjd fsm sidfjsm sid .",
+            body: "Lorem epsuem.. This Review has also ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf. ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf. ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj. ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf. ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf.",
+            vreviews: [{ title: "Check" }, { title: "Check 2" }, { title: "Check 2" }, { title: "Check 2" }]
+        },
+    ];
+
+}]);
+
+
+
+
 module.factory("dataService", function ($http, $q) {
-    var movies = [];
+    var _movies = [];
+    var _selectedmovies = [];
+    var _selectedmovie = [];
 
-    var deferred = $q.defer();
+    var _getMovies = function () {
+        var deferred = $q.defer();
+        $http.get("/api/movies/get").then(function (result) {
+            angular.copy(result.data, _movies);
+            angular.copy(result.data, _selectedmovies);
+            angular.copy(_selectedmovies.filter(function (movie) { return movie.Title === _selectedmovies[0].Title; }), _selectedmovie);
 
-    var getMovies = function () {
-        $http.get("/api/movies/getbylanguage/" + $scope.selectedItem).then(function (result) {
-            angular.copy(result.data, movies);
+            deferred.resolve();
         }, function () {
-
-        }).then(function () {
-            $scope.isBusy = false;
+            deferred.reject();
         });
+
+        return deferred.promise;
     };
     return {
-
+        movies: _movies,
+        getMovies: _getMovies,
+        selectedmovies: _selectedmovies,
+        selectedmovie: _selectedmovie
     };
 
 });
@@ -77,35 +124,11 @@ module.config(function ($routeProvider) {
         controller: "homeIndexController",
         templateUrl: "/AngularTemplates/movieView.html",
     });
-    $routeProvider.when("/moviereview", {
+    $routeProvider.when("/moviereview/:movieTitle", {
        controller: "reviewIndexController",
         templateUrl: "/AngularTemplates/reviewView.html"
     });
 
     $routeProvider.otherwise({ redirectTo: "/" });
 });
-
-    module.controller('reviewIndexController', ['$scope', function($scope){
-        $scope.name = "Review Page";
-        $scope.data = [
-            {
-                image: "bahu.jpg",
-                cast: "Anushka, Prabhas",
-                director: "Rajamouli",
-                producer: "Shobhu Yarlagadda",
-                userrating: 4,
-                desireviewrating: 3
-            }
-        ];
-
-        $scope.reviewdata = [
-            {
-                title: "I call it a great Ripoff! Most of us would call it a Blockbuster!",
-                introduction: "Introduction content. Lorem epsum. Chedsfw sdifjsifs dsfisjdf. Lorem epsuem.. This Review has also ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif. dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf. ksjdi dsfjiwsjd fsm sidfjsm sid .",
-                body: "Lorem epsuem.. This Review has also ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf. ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf. ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj. ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf. ksjdi dsfjiwsjd fsm sidfjsm sid fjsdf  isdjflsdfj isdf jsi fsjl jsldifjd sljfidsljf sdfjlsdifj sldfj dslfijdslfdsjfdslifj dslifj sdlfjds lfdslfjdslfjdslifjdslifjdsljfldsijfsdfdsf dsf dsljfdsljfdsljfdsfijdsf sdlf jdslif jdslfj dslfjdslfj dslifj dslfj dslif jdsljf dsljf ldsjf ldsj flds fjldsjfldsijfdsljfldsijfldsijf.",
-                vreviews: [{ title: "Check" }, { title: "Check 2" }, { title: "Check 2" }, { title: "Check 2" }]
-            },
-        ];
-
-    }]);
 
